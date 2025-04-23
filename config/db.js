085d0f7c7c3bf3f81d2b22657a8e4cd75ce04406
aom <yaowaretkw@gmail.com>
2025-04-23 14:44:00 +0700

    create project

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
 // config/db.js
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'mymaria@2025',
-  database: 'shopdb',
-  connectionLimit: 10,
-  port: 5210
-});
-
 // const pool = mysql.createPool({
-//     host: 'sql108.infinityfree.com',
-//     user: 'if0_38813002',
-//     password: 'CQ4DkXaCvs',
-//     database: 'if0_38813002_shopdb',
-//     port: 3306
-//   });
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'mymaria@2025',
+//   database: 'shopdb',
+//   connectionLimit: 10,
+//   port: 5210
+// });
+
+const pool = mysql.createPool({
+    host: 'sql108.infinityfree.com',
+    user: 'if0_38813002',
+    password: 'CQ4DkXaCvs',
+    database: 'if0_38813002_shopdb',
+    port: 3306
+  });
 
 pool.getConnection((err, connection) => {
   if (err) {
